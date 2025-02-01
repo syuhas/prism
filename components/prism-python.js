f30abb9654e67f27572ed398247c089758cf9d1e
syuhas <syuhas22@gmail.com>
2025-02-01 13:22:22 -0500

    custom okaidia vscode theme

--- components/prism-python.js
+++ components/prism-python.js
@@ -4,6 +4,11 @@ Prism.languages.python = {
 		lookbehind: true,
 		greedy: true
 	},
+
+	'dbops': /\b(?:filter|query|commit|close|remove|rollback|and_|add|first|one)\b/,
+
+	'builtin': /\b(?:__import__|abs|all|any|apply|ascii|basestring|bin|bool|buffer|bytearray|bytes|callable|chr|classmethod|cmp|coerce|compile|complex|delattr|dict|dir|divmod|enumerate|eval|execfile|file|filter|float|format|frozenset|getattr|globals|hasattr|hash|help|hex|input|int|intern|isinstance|issubclass|iter|list|locals|long|map|max|memoryview|min|next|object|oct|open|ord|pow|property|range|raw_input|reduce|reload|repr|reversed|round|set|setattr|slice|sorted|staticmethod|str|sum|super|tuple|type|unichr|unicode|vars|xrange|zip)\b/,
+
 	'string-interpolation': {
 		pattern: /(?:f|fr|rf)(?:("""|''')[\s\S]*?\1|("|')(?:\\.|(?!\2)[^\\\r\n])*\2)/i,
 		greedy: true,
@@ -24,6 +29,7 @@ Prism.languages.python = {
 					rest: null
 				}
 			},
+			'bracket': /[{}]/,
 			'string': /[\s\S]+/
 		}
 	},
@@ -52,8 +58,44 @@ Prism.languages.python = {
 			'punctuation': /\./
 		}
 	},
+	
+	'variable': {
+		pattern: /^\s*(\w+)\s*(?==)/m,
+		lookbehind: false,
+		alias: 'variable',
+	},
+
+	
+
+	'library': {
+		pattern: /\b(?:os|sys|re|json|datetime|time|random|math|numpy|pandas|matplotlib|seaborn|scipy|sklearn|tensorflow|keras|torch|torchvision|cv2|typing|List|Dict|Union|boto3.session|boto3.resource|boto3.client|boto3.s3.transfer|botocore.exceptions|ClientError|boto3|loguru|logger|logging|sqlalchemy.orm|sqlalchemy.ext.declarative|sqlalchemy.ext.declarative.api|sqlalchemy.ext.declarative.base|sqlalchemy.ext.declarative|sqlalchemy|declarative_base|create_engine|select|relationship|scoped_session|sessionmaker|Column|Integer|String|DateTime|ForeignKey|Table|MetaData|create_all|DECIMAL|BigInteger|concurrent.futures|ThreadPoolExecutor|itertools|islice|Exception)\b/,
+		alias: 'library',
+		greedy: true
+	},
+
+	'classcall' : {
+		pattern: /([A-Z0-9]+){2,}/g,
+		alias: 'classcall',
+		greedy: true
+	},
+
+	'functioncall': {
+		pattern: /([a-zA-Z]+)(?=\()/m,
+		alias: 'functioncall',
+		greedy: false
+	},
+
+
+	'innerparen': {
+		// pattern: /{}()/,
+		pattern: /(?<=[(]([^()])*)\(|\)(?=[^()]*[)])/g,
+		alias: 'innerparen',
+		greedy: true
+	},
+
+	'classdef': /\b(?:class|def)\b/,
 	'keyword': /\b(?:_(?=\s*:)|and|as|assert|async|await|break|case|class|continue|def|del|elif|else|except|exec|finally|for|from|global|if|import|in|is|lambda|match|nonlocal|not|or|pass|print|raise|return|try|while|with|yield)\b/,
-	'builtin': /\b(?:__import__|abs|all|any|apply|ascii|basestring|bin|bool|buffer|bytearray|bytes|callable|chr|classmethod|cmp|coerce|compile|complex|delattr|dict|dir|divmod|enumerate|eval|execfile|file|filter|float|format|frozenset|getattr|globals|hasattr|hash|help|hex|id|input|int|intern|isinstance|issubclass|iter|len|list|locals|long|map|max|memoryview|min|next|object|oct|open|ord|pow|property|range|raw_input|reduce|reload|repr|reversed|round|set|setattr|slice|sorted|staticmethod|str|sum|super|tuple|type|unichr|unicode|vars|xrange|zip)\b/,
+	// 'builtin': /\b(?:__import__|abs|all|any|apply|ascii|basestring|bin|bool|buffer|bytearray|bytes|callable|chr|classmethod|cmp|coerce|compile|complex|delattr|dict|dir|divmod|enumerate|eval|execfile|file|filter|float|format|frozenset|getattr|globals|hasattr|hash|help|hex|input|int|intern|isinstance|issubclass|iter|len|list|locals|long|map|max|memoryview|min|next|object|oct|open|ord|pow|property|range|raw_input|reduce|reload|repr|reversed|round|set|setattr|slice|sorted|staticmethod|str|sum|super|tuple|type|unichr|unicode|vars|xrange|zip)\b/,
 	'boolean': /\b(?:False|None|True)\b/,
 	'number': /\b0(?:b(?:_?[01])+|o(?:_?[0-7])+|x(?:_?[a-f0-9])+)\b|(?:\b\d+(?:_\d+)*(?:\.(?:\d+(?:_\d+)*)?)?|\B\.\d+(?:_\d+)*)(?:e[+-]?\d+(?:_\d+)*)?j?(?!\w)/i,
 	'operator': /[-+%=]=?|!=|:=|\*\*?=?|\/\/?=?|<[<=>]?|>[=>]?|[&|^~]/,
